757519508b6b78bdfaca92042cdeac47421428ac
Sanna Luukkonen <39089796+skis84@users.noreply.github.com>
2021-06-22 09:22:15 +0300

    Update second.py

--- second.py
+++ second.py
@@ -4,5 +4,15 @@ import sgraph
 print("there is something")
 print("test test test")
 
+
+print("there is something")
+print("test test test")
+
+print("there is something")
+print("test test test")
+
+print("there is something")
+print("test test test")
+
 def testfunc():
   print("test")
